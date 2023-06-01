f9978fff2c0ba58ae38d3e3890ed84e35d796c01
CristianSalinas96 <cristiansalinas1996@icloud.com>
2023-06-01 13:16:02 -0400

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Cristian Salinas on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
